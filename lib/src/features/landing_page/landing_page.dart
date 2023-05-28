dde6659396390522aff0eda2cb902c1de108b51a
viveky259259 <viveky259259@gmail.com>
2023-05-28 19:21:11 +0530

    Updated landing page UI

--- lib/src/features/landing_page/landing_page.dart
+++ lib/src/features/landing_page/landing_page.dart
@@ -13,27 +13,83 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Landing Page')),
-      body: Column(children: [
-        ElevatedButton(
-            onPressed: () async {
+      appBar: AppBar(title: const Text('Landing Page')),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          const Text('Start'),
+          const SizedBox(
+            height: 32,
+          ),
+          GestureDetector(
+            onTap: () => debugPrint('To implement new file'),
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(4)),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Icon(Icons.file_copy),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Text('New file...')
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          InkWell(
+            onTap: () async {
               final result = await FilePicker.platform.getDirectoryPath();
-              print(result);
-                if (result != null) {
-                context.router.push(EditorRoute(filePickerResult: result));
-              }
-          
-            },
-            child: Text('Open Folder')),
-        ElevatedButton(
-            onPressed: () async {
-              final result = await FilePicker.platform.pickFiles();
+
               if (result != null) {
                 context.router.push(EditorRoute(filePickerResult: result));
               }
             },
-            child: Text('Open File')),
-      ]),
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(4)),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Icon(Icons.file_open),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Text('Open...')
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const Text('Recent'),
+          const SizedBox(
+            height: 8,
+          ),
+          Row(
+            children: const [
+              Text('Project Name'),
+              SizedBox(
+                width: 16,
+              ),
+              Text('Project path')
+            ],
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          TextButton(onPressed: () {}, child: const Text('More'))
+        ]),
+      ),
     );
   }
 }
